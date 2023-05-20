59284bc2bc25edd3f836e21afebcb8a2d726fe8c
imtiaz-ahmmed <imtiazahmmed00@gmail.com>
2023-05-20 22:52:53 +0600

    delete operation created

--- index.js
+++ index.js
@@ -75,6 +75,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await detailsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
